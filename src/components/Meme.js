f363190de619ba3e5bafc84387f4a6f789b894fe
Fabien Chareun <cacrfabfr@gmail.com>
2021-11-24 13:41:51 +0100

    state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,43 @@
+import memeData from '../memeData'
+
 export default function Meme() {
-  const testFunction = (e) => {
-    e.preventDefault();
-    console.log('teeeeeest');
+
+  const getRandomNumberBetween = (min,max) => {
+    return Math.floor(Math.random()*(max-min+1)+min);
   }
+
+  const displayLink = (e) => {
+    e.preventDefault();
+    const memesArray = memeData.data.memes
+    const randomNumber = getRandomNumberBetween(0, memesArray.length-1)
+    const memesUrl = memesArray[randomNumber].url
+
+    console.log(memesUrl);
+    return memesUrl;
+  }
+
   return (
-      <main>
-          <form className="form">
-              <input 
-                  type="text"
-                  placeholder="Top text"
-                  className="form--input"
-              />
-              <input 
-                  type="text"
-                  placeholder="Bottom text"
-                  className="form--input"
-              />
-              <button 
-                  className="form--button"
-                  onClick={testFunction}
-              >
-                  Get a new meme image 🖼️
-              </button>
-          </form>
-      </main>
+    <main>
+      <form className="form">
+        <input 
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+        />
+        <input 
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+       />
+        <button 
+          className="form--button"
+          onClick={displayLink}
+        >
+          Get a new meme image 🖼️
+        </button>
+
+        <p>{displayLink}</p>
+      </form>
+    </main>
   )
 }
